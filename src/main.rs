94526d3cf26c0709e956c00878eff9daff11aec1
Jamie Holloway <jamie@jamieholloway.com>
2019-08-10 21:50:32 -0700

    updates

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello rusty");
+    use std::mem;
+    //println!("Hello rusty");
+    let a:u8 = 123; // unsigned integer 8bits immutable
+    let mut b:i8 = -123; // integer 8bits mutable
+    println!("a={} b={}", a, b);
+    b = 42;
+    println!("a={} b={}", a, b);
+    let c:u8 = a;
+    println!("a={} b={} c={}", a, b, c);
+    let d = 123456789;
+    println!("b={} size = {} bytes", d, mem::size_of_val(&b));    
 }
