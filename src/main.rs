457b7f22e0f68a618428514e19b9d0864a5db1eb
Jamie Holloway <jamie@jamieholloway.com>
2019-08-11 21:17:42 -0700

    updates

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,20 @@ mod stack_and_heap;
 fn main() 
 {
     //funcs::data_types();  // function names need to be snake case per compiler
-    stack_and_heap::memory();
+    //stack_and_heap::memory();
+    if_statement();
+}
+
+fn if_statement()
+{
+    let temp = 40;
+
+    if temp > 30
+    {
+        println!("temp is gt 30")
+    }
+    else 
+    {
+        println!("temp is le 30")
+    }
 }
